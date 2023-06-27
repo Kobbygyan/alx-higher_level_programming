3585b867a1d922b6a0240452cf6b7f922e6f313c
Kobbygyan <kobbygyan86@gmail.com>
2023-06-26 18:51:55 -0700

    a & b

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Divide two integers and print the result.
+
+    Args:
+        a (int): The numerator.
+        b (int): The denominator.
+
+    Returns:
+        float or None: The result of the division if successful, None otherwise.
+    """
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
