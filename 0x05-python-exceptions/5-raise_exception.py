86dfeff1ee7b06e0dd494b29079ddf470381a28a
Kobbygyan <kobbygyan86@gmail.com>
2023-06-26 19:01:37 -0700

    exception raised

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """Raise a type exception.
+
+    Raises:
+        TypeError: Always raises a type exception.
+
+    """
+    raise TypeError("This is a type exception.")
