7cdd8326e8372d24447aac19ae9a89aa7c0b27db
Kobbygyan <kobbygyan86@gmail.com>
2023-06-27 18:26:24 -0700

    private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Working on OOP with Python"""
+
+
+class Square:
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """Initialize the class.
+
+        Args:
+            size (int): Size of the square.
+        """
+        self.__size = size
