06bee6cbb19befe1bde626ae3c9e0cbe27129369
Kobbygyan <kobbygyan86@gmail.com>
2023-09-01 11:04:51 -0700

    Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- Lists the number of records with the same score in the table second_table.
+-- Records are ordered by descending count.
+SELECT score, name
+FROM second_table
+HAVING name IS NOT NULL
+ORDER BY score DESC;
