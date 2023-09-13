dfecec8fc3082acc9f630edad286d0a61ea40509
Kobbygyan <kobbygyan86@gmail.com>
2023-09-12 19:05:26 -0700

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-
-// Get the two arguments using array destructuring
-const [argv1, argv2] = process.argv.slice(2);
-
-// Check if both arguments are defined
-if (typeof argv1 !== 'undefined' && typeof argv2 !== 'undefined') {
-  // Use console.log(...) to print the formatted output
-  console.log(argv1 + ' is ' + argv2);
-} else {
-  console.log('Please provide two arguments.');
-}
+const { argv } = require('process');
+const argv1 = argv[2]; const argv2 = argv[3];
+console.log(argv1 + ' is ' + argv2);
