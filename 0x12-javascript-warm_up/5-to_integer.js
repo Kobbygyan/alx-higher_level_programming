1a7c3b79ec598ef0af8729abbab5db154f258ad2
Kobbygyan <kobbygyan86@gmail.com>
2023-09-24 18:12:48 -0700

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Get the first argument from the command line
+const arg1 = process.argv[2];
+
+// Use the parseInt function to convert the argument to an integer
+const number = parseInt(arg1, 10);
+
+// Check if the conversion resulted in a valid integer
+if (!isNaN(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log("Not a number");
+}
