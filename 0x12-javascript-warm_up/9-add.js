09f48e4b537d1a668e017f67a7046dc7099fd195
Kobbygyan <kobbygyan86@gmail.com>
2023-09-24 18:48:05 -0700

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,18 +1,3 @@
 #!/usr/bin/node
-
-// Define the add function with the given prototype
-function add (a, b) {
-  return a + b;
-}
-
-// Get the two integers from the command line arguments
-const arg1 = parseInt(process.argv[2], 10);
-const arg2 = parseInt(process.argv[3], 10);
-
-// Check if the conversion resulted in valid integers
-if (!isNaN(arg1) && !isNaN(arg2)) {
-  const result = add(arg1, arg2);
-  console.log(result);
-} else {
-  console.log('Invalid input. Please provide two integers.');
-}
+const { argv } = require('process');
+console.log(parseInt(argv[2]) + parseInt(argv[3]));
