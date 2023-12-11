df097ff86ab6ad2586de2fb78911972ac483d398
Kobbygyan <kobbygyan86@gmail.com>
2023-12-11 15:18:46 -0800

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
-
-function factorial (n) {
-  if (isNaN(n) || n < 0) {
-    return 1;
+function Factorial (number) {
+  if (!number) {
+    return (1);
   }
-
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
+  if (number !== 0) {
+    return (number * Factorial(number - 1));
   }
 }
-
-const num = parseInt(process.argv[2]);
-
-console.log({factorial(num)});
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+console.log(Factorial(number));
