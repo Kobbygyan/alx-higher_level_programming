bf5f6b833907e4fe473897bf64038e4ce732a7b5
Kobbygyan <kobbygyan86@gmail.com>
2023-12-11 15:04:49 -0800

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,17 +1,3 @@
 #!/usr/bin/node
-
-function add (a, b) {
-  return a + b;
-}
-
-const args = process.argv.slice(2);
-
-const num1 = parseInt(args[0]);
-const num2 = parseInt(args[1]);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  const result = add(num1, num2);
-  console.log(`The addition of ${num1} and ${num2} is: ${result}`);
-} else {
-  console.log('Invalid input. Please provide two integers.');
-}
+const { argv } = require('process');
+console.log(parseInt(argv[2]) + parseInt(argv[3]));
